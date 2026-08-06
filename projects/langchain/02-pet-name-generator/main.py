8c11ebf954e4d26dacf8a70c4c8d3c083be9225e
bhimanshukalra <bhimanshukalra@gmail.com>
2026-08-06 23:49:42 +0530

    Refactor code structure for improved readability and maintainability

--- projects/langchain/02-pet-name-generator/main.py
+++ projects/langchain/02-pet-name-generator/main.py
@@ -1,5 +1,22 @@
+import streamlit as st
+import helper
+
+
 def main():
-    print("Hello from 02-pet-name-generator!")
+    st.title("🐶 Pets Name Generator")
+
+    animal_type = st.sidebar.selectbox(
+        "What is your pet?", ("Dog", "Cat", "Hamster", "Rat", "Snake", "Lizard", "Cow")
+    )
+
+    pet_color = st.sidebar.text_area(
+        label=f"What color is your {animal_type}?",
+        max_chars=25,
+    )
+
+    if pet_color:
+        response = helper.generate_pet_name(animal_type, pet_color)
+        st.markdown(response)
 
 
 if __name__ == "__main__":
