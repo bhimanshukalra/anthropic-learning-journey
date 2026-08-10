9428b4d899283c6a19e91706bffacf236e02052c
bhimanshukalra <bhimanshukalra@gmail.com>
2026-08-10 17:14:18 +0530

    Add weather label node to LangGraph demo

--- projects/langgraph/demo/main.py
+++ projects/langgraph/demo/main.py
@@ -5,6 +5,7 @@ from typing import TypedDict
 class TemperatureState(TypedDict):
     temp_celsius: float
     temp_fahrenheit: float
+    weather_status: str
 
 
 def convert_temp(state: TemperatureState) -> TemperatureState:
@@ -15,13 +16,30 @@ def convert_temp(state: TemperatureState) -> TemperatureState:
     return state
 
 
+def label_weather(state: TemperatureState) -> TemperatureState:
+    fahrenheit = state["temp_fahrenheit"]
+
+    if fahrenheit < 50:
+        state["weather_status"] = "Cold"
+    elif 50 <= fahrenheit < 77:
+        state["weather_status"] = "Mild"
+    elif 77 <= fahrenheit < 95:
+        state["weather_status"] = "Hot"
+    else:
+        state["weather_status"] = "Extreme Heat"
+
+    return state
+
+
 def main():
     graph = StateGraph(state_schema=TemperatureState)
 
     graph.add_node("convert_temp", convert_temp)
+    graph.add_node("label_weather", label_weather)
 
     graph.add_edge(START, "convert_temp")
-    graph.add_edge("convert_temp", END)
+    graph.add_edge("convert_temp", "label_weather")
+    graph.add_edge("label_weather", END)
 
     workflow = graph.compile()
 
